ef1807f525469000057ef956c32754f4eb18a61b
tanya-mishra10 <replytotanya@gmail.com>
2025-04-26 20:06:01 +0530

    using GUI create button on frame

---
+++ src/javaLibraryExamples/Simpleguil.java
@@ -0,0 +1,15 @@
+package javaLibraryExamples;
+
+import javax.swing.*;
+
+public class Simpleguil {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("click me");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.getContentPane().add(button);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
