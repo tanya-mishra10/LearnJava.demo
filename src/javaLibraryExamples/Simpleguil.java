73192160ea2761e141660bcbaf9d23ff2eb07bc0
tanya-mishra10 <replytotanya@gmail.com>
2025-04-27 12:17:10 +0530

    add event handling

--- src/javaLibraryExamples/Simpleguil.java
+++ src/javaLibraryExamples/Simpleguil.java
@@ -1,15 +1,29 @@
 package javaLibraryExamples;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Simpleguil {
+public class Simpleguil implements ActionListener {
+    private JButton button;
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JButton button = new JButton("click me");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Simpleguil sm = new Simpleguil();
+        sm.go();
+    }
+        public void go () {
+            JFrame frame = new JFrame();
+            button = new JButton("click me");
+            button.addActionListener(this);
 
-        frame.getContentPane().add(button);
-        frame.setSize(300, 300);
-        frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.getContentPane().add(button);
+            frame.setSize(300, 300);
+            frame.setVisible(true);
+        }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        button.setText("I have been clicked");
     }
 }
+
